bda8e33b48b77ab3340e13fa47a307c6b034aae7
Wanzz001 <wandyridwansyah75@gmail.com>
2024-09-12 14:22:09 +0700

    Add image

--- app/src/main/java/com/greentech/monitorsampah/screen/HomeScreen.kt
+++ app/src/main/java/com/greentech/monitorsampah/screen/HomeScreen.kt
@@ -1,9 +1,15 @@
 package com.greentech.monitorsampah.screen
 
+import android.Manifest
+import android.content.Intent
+import android.provider.MediaStore
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -13,12 +19,20 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.greentech.monitorsampah.DataDummy
 import com.greentech.monitorsampah.R
 import com.greentech.monitorsampah.dataDummy
@@ -44,16 +58,57 @@ fun HomeScreen(modifier: Modifier = Modifier, navController: NavHostController) 
     }
 }
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun ListSampah(modifier: Modifier = Modifier, data: DataDummy, item: Int) {
+    val context = LocalContext.current
+    val permissionState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.CAMERA
+        )
+    )
+    LaunchedEffect(Unit) {
+        permissionState.launchMultiplePermissionRequest()
+    }
+    var showDialog by remember { mutableStateOf(false) }
     Card(modifier = modifier
         .fillMaxWidth()
-        .padding(vertical = 7.dp, horizontal = 10.dp)) {
+        .padding(vertical = 7.dp, horizontal = 10.dp)
+        .clickable {
+            showDialog = true
+        }) {
         Column(modifier = Modifier.padding(14.dp)) {
             Text(text = "Tong Sampah $item", fontWeight = FontWeight.ExtraBold)
             Text(text = data.status)
             Text(text = "Koordinat : ${data.coordinate.first}, ${data.coordinate.second}")
         }
+    }
+    if (showDialog) {
+        AlertDialog(onDismissRequest = {
+            showDialog = false
+        },
+            title = {
+                Text(text = "Ambil Foto")
+            },
+            text = {
+                Text(text = "Ambil foto menggunakan kamera")
+            },
+            confirmButton = {
+                Button(onClick = {
+                    showDialog = false
+                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    context.startActivity(intent)
+                }) {
+                    Text(text = "Ambil")
+                }
+            },
+            dismissButton = {
+                Button(onClick = {
+                    showDialog = false
+                }) {
+                    Text(text = "Batal")
+                }
+            })
     }
 }
 
--- app/src/main/java/com/greentech/monitorsampah/screen/MapsScreen.kt
+++ app/src/main/java/com/greentech/monitorsampah/screen/MapsScreen.kt
@@ -2,7 +2,10 @@ package com.greentech.monitorsampah.screen
 
 import android.Manifest
 import android.content.pm.PackageManager
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,7 +18,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.navigation.NavHostController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
@@ -25,9 +32,10 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
-import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerInfoWindowContent
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.greentech.monitorsampah.R
 import com.greentech.monitorsampah.dataDummy
 import com.greentech.monitorsampah.ui.theme.Green
 
@@ -68,11 +76,11 @@ fun MapsScreen(modifier: Modifier = Modifier, navController: NavHostController) 
                             Manifest.permission.ACCESS_COARSE_LOCATION
                         ) != PackageManager.PERMISSION_GRANTED
                     ) {
-                        fusedLocationClient.lastLocation.addOnSuccessListener { loc ->
-                            loc?.let {
-                                myLocation = LatLng(it.latitude, it.longitude)
-                                cameraPositionState.position = CameraPosition.fromLatLngZoom(myLocation!!, 15f)
-                            }
+                    }
+                    fusedLocationClient.lastLocation.addOnSuccessListener { loc ->
+                        loc?.let {
+                            myLocation = LatLng(it.latitude, it.longitude)
+                            cameraPositionState.position = CameraPosition.fromLatLngZoom(myLocation!!, 15f)
                         }
                     }
                 } else {
@@ -89,10 +97,14 @@ fun MapsScreen(modifier: Modifier = Modifier, navController: NavHostController) 
             ) {
                 dataDummy.forEach { data ->
                     val location = LatLng(data.coordinate.first, data.coordinate.second)
-                    Marker(
+                    MarkerInfoWindowContent(
                         state = MarkerState(position = location),
-                        title = "Tong Sampah ${dataDummy.indexOf(data) + 1}",
-                    )
+                    ) {
+                        Column {
+                            Text(text = "Tong Sampah ${dataDummy.indexOf(data) + 1}", fontWeight = FontWeight.ExtraBold, color = Color.Black)
+                            Image(painter = painterResource(id = R.drawable.logo), contentDescription = null, modifier = modifier.size(150.dp))
+                        }
+                    }
                 }
             }
         } else {
